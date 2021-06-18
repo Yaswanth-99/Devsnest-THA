fbb7cc3ea5f8e10310fd2b6b1e62dbdf13b8cfe0
Yaswanth-99 <52022393+Yaswanth-99@users.noreply.github.com>
2021-06-18 10:18:38 +0530

    Update objects.js

--- THA 7(Objects)/objects.js
+++ THA 7(Objects)/objects.js
@@ -39,3 +39,28 @@ function cyl(r,h){
     return (Math.PI * 2*r*h);
 }
 console.log(cyl(3.2,4.5).toFixed(4));
+
+/*6. Write a JavaScript program to sort an array of JavaScript objects.  Sample Object : var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, 
+{ title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
+Expected Output: [[object Object] { author: "Walter Isaacson", libraryID: 4264, title: "Steve Jobs" },
+[object Object] { author: "Suzanne Collins", libraryID: 3245, title: "Mockingjay: The Final Book of The Hunger Games" }, 
+[object Object] { author: "The Road Ahead", libraryID: 1254, title: "Bill Gates" }]*/
+
+var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 },
+               { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, 
+               { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
+
+function ord(a, b) {
+  if (a.title<b.title) {
+    return -1;
+  }
+  if (a.title>b.title) {
+    return 1;
+  }
+  return 0;
+}
+console.log(
+  library.sort((a,b) => {
+    ord(a,b);
+  })
+);
